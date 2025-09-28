5f1cfdebfe0768341a0501d9b19133780960b62c
Lucas Soares <142176643+Lucc337@users.noreply.github.com>
2025-09-28 10:30:23 -0300

    Turismos - inicial

---
+++ feature eventos/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'eventos'
+
+urlpatterns = [
+    path('', views.lista_eventos, name='lista'),
+    path('calendario/', views.calendario, name='calendario'),
+    path('form/', views.novo_evento, name='form'),
+    path('<int:pk>', views.detalhe_evento, name='detalhe'),
+    path('<int:pk>/editar/', views.editar_evento, name='editar'),
+    path('<int:pk>/inscricao/', views.inscricao_evento, name='inscricao'),
+]
